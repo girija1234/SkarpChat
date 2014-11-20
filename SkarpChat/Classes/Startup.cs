269d59da16e58f02c12a993d73c91aab8ddb87c0
Gowda Yogesh <Yogesh.Gowda@ingramcontent.com>
2014-11-20 13:53:37 -0600

    Initial Commit

---
+++ SkarpChat/Classes/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(SkarpChat.Startup))]
+namespace SkarpChat.Classes
+{
+    public class Startup
+    {
+   
+    }
+}
+
---
+++ SkarpChat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SkarpChat.MvcApplication" Language="C#" %>
